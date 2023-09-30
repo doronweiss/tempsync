20e8aaf23f7f92aba9743bb4fb1f307899e5442e
doron <doron@10fs.co.il>
2023-09-30 14:13:38 +0300

    initial commit

---
+++ calcs.py
@@ -0,0 +1,54 @@
+from typing import List
+
+import pandas as pd
+import numpy as np
+
+def multLists (lref, lshort, startIdx):
+    sz : int = len(lshort)
+    sum : float=0.0
+    for idx in range(sz):
+        sum += lref[idx+startIdx] * lshort[idx] / sz
+    return sum
+def subLists (lref, lshort, startIdx):
+    sz : int = len(lshort)
+    sum : float=0.0
+    for idx in range(sz):
+        sum += (lref[idx+startIdx] - lshort[idx]) / sz
+    return sum
+def corrSeries(ser1, ser2, method : int):
+    if method == 1:
+        sz = len (ser1)
+        sliceSz = int(sz/10)
+        slice1 = list(ser1[0:sliceSz])
+        slice2 = list(ser2[0:sliceSz])
+        paddingl: list[float] = [0.0 for i in range(sliceSz)]
+        paddingr: list[float] = [0.0 for i in range(sliceSz)]
+        slice1 = paddingl + slice1 + paddingr;
+        # slice1 = slice1 + slice1 + slice1
+        resl=[]
+        for ind in range(sliceSz*2):
+            corrRes = multLists(slice1, slice2, ind)
+            resl.append(corrRes)
+            if not ind % 100:
+                print ("ind: {0}\n".format(ind))
+        return resl
+    if method == 2:
+        sz = len (ser1)
+        sliceSz = int(sz/100)
+        slice1 = [ser1[i] for i in range(sz) if i % sliceSz == 0]
+        slice2 = [ser2[i] for i in range(sz) if i % sliceSz == 0]
+        actLen = len(slice1)
+        paddingl: list[float] = [0.0 for i in range(actLen)]
+        paddingr: list[float] = [0.0 for i in range(actLen)]
+        slice1 = paddingl + slice1 + paddingr;
+        resl=[]
+        for ind in range(actLen*2):
+            #corrRes = subLists(slice1, slice2, ind)
+            corrRes = multLists(slice1, slice2, ind)
+            resl.append(corrRes)
+            if not ind % 100:
+                print ("ind: {0}\n".format(ind))
+        midx = resl.index(max(resl))
+        print("len: {0} max: {1}\n".format(actLen, midx))
+        return resl
+
---
+++ main.py
@@ -0,0 +1,28 @@
+# This is a sample Python script.
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import calcs
+# Press Ctrl+F5 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+data = pd.read_csv(r'c:\Users\User\OneDrive\Projects\BennyGrannot\benny.csv', encoding='utf-8')
+
+# fig1, ax = plt.subplots()
+# ax.plot(data.CH1.values)
+# ax.plot(data.CH2.values)
+# ax.plot(data.CH3.values)
+# ax.plot(data.CH4.values)
+# ax.plot(data.CH5.values)
+# fig1.show()
+# exit(0)
+
+res = calcs.corrSeries(data.CH1.values, data.CH2.values, 2)
+fig2, ax = plt.subplots()
+ax.plot(res)
+fig2.show()
+
+#plt.close(fig1)
+plt.close(fig2)
+
